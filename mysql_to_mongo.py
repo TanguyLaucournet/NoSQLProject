277a9d52ec6944f173a5062cfe2aa8a8b310db9c
liviator <76626503+liviator@users.noreply.github.com>
2021-03-30 16:26:01 +0200

    Update mysql_to_mongo.py

--- mysql_to_mongo.py
+++ mysql_to_mongo.py
@@ -1,1 +1,15 @@
-# MySQL to Mongo
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+
+ 
+
+sqlEngine       = create_engine('mysql+pymysql://root:password@127.0.0.1/nosql', pool_recycle=3600) #root = l'user de votre workbench, password votre mot de pase et nosql la db
+dbConnection    = sqlEngine.connect()
+frame           = pd.read_sql("select * from jsonfile", dbConnection); #jsonfile doit etre remplacé en fonction du nom de la table
+pd.set_option('display.expand_frame_repr', False)
+print(frame)
+
+ 
+
+dbConnection.close()
