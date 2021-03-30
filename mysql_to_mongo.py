7ef6eeaf699954a4558c56113d402585b1d1362e
Tanguy Laucournet <40137577+TanguyLaucournet@users.noreply.github.com>
2021-03-30 16:28:54 +0200

    Update mysql_to_mongo.py

--- mysql_to_mongo.py
+++ mysql_to_mongo.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
+import pymongo
 
  
 
@@ -10,6 +11,23 @@ frame           = pd.read_sql("select * from jsonfile", dbConnection); #jsonfile
 pd.set_option('display.expand_frame_repr', False)
 print(frame)
 
- 
+ dbConnection.close()
 
-dbConnection.close()
+
+#Insert in mongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+dblist = myclient.list_database_names()
+if "database" in dblist:
+    db = myclient.mydatabase
+    print("The database exists.")
+else:
+    db = myclient["database"]
+    print("The database is created.")
+
+collection = db['collection_name']
+frame2.reset_index(inplace=True)
+data_dict = frame2.to_dict("records")
+# Insert collection
+collection.insert_many(data_dict)
